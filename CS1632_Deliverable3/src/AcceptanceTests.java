911b0fb5a7a6807b6f1065bb9503cd67d3c7f4df
Joe Meszar <mesz@outk.st>
2016-02-19 15:17:40 -0500

    Created netbeans project solution that Cyrus will most likely laugh at me for...

---
+++ CS1632_Deliverable3/src/AcceptanceTests.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Joe Meszar (jwm54@pitt.edu) | Cyrus Ramavarapu (crr41@pitt.edu)
+ */
+public class AcceptanceTests {
+    
+    public static void main (String[] args) {
+        
+    }
+}
